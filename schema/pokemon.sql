f5bc2bf32d0e39d6f3e123c2ff6baad718eedf0a
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-07-19 15:37:22 -0700

    code from exam added and rendering

---
+++ schema/pokemon.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS pokemon;
+
+CREATE TABLE pokemon
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(225)
+);
---
+++ server.js
@@ -0,0 +1,102 @@
+'use strict';
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3005;
+
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const morgan = require('morgan');
+
+const app = express();
+const database = new pg.Client(process.env.DATABASE_URL);
+
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/login', connectionTest);
+app.get('/', getPokeNames);
+app.post('/add', addFavorites);
+app.get('/favorites', getFavorites);
+app.use('*', routeNotFound);
+app.use(bigError);
+
+//----------Connection Test
+function connectionTest(req, res){
+    let url = 'https://fontmeme.com/permalink/200718/724a6d29d63468457272b3a6112fd84b.png'
+    res.status(200).render('pages/login', {route: '/favorites', linkToRoute: 'Login', url: url});
+}
+
+//----------Show Pokemon Names
+function getPokeNames(req, res){
+    const API = 'https://pokeapi.co/api/v2/pokemon';
+
+    superagent.get(API).then(data => {
+        let getPokeArr = data.body.results.map(targetNames => {
+            let newArr = new Pokemon(targetNames).name;
+            return newArr;
+        })
+        let url = 'https://fontmeme.com/permalink/200718/724a6d29d63468457272b3a6112fd84b.png';
+
+        getPokeArr.sort();
+
+        res.status(200).render('pages/show', {names: getPokeArr, route: '/favorites', linkToRoute: 'Favorites', url: url});
+
+    }).catch(error => bigError(error, res));
+}
+
+//----------Pokemon Constructor
+function Pokemon(obj){
+    this.name = obj.name;
+}
+
+//----------Add Pokemon to Favorites
+function addFavorites(req, res){
+    let saveToDB = [req.body.name];
+    const SQL = `INSERT INTO pokemon (name)  VALUES ($1);`;
+
+    database.query(SQL, saveToDB).then(data => {
+        let row = data.rows;
+    
+        res.status(200).redirect('/');
+
+    }).catch(error => bigError(error, res));
+}
+
+//----------Show Favorite Pokemon
+function getFavorites(req, res){
+    const SQL = 'SELECT * FROM pokemon;';
+
+    database.query(SQL).then(data => {
+        let row = data.rows.map(targetNames => {
+            let newArr = new Pokemon(targetNames).name;
+            let twoArr = newArr.name;
+            return newArr;
+        })
+        let url = 'https://fontmeme.com/permalink/200718/62a4f2b98f2af5ac4721709ccb01b039.png';
+
+        res.status(200).render('pages/favorites', {names: row, route: '/', linkToRoute: 'Home', url: url});
+
+    }).catch(error => bigError(error, res));
+}
+
+//----------404 Error
+function routeNotFound(req, res) {
+    res.status(404).send('Route NOT Be Found!');
+}
+
+//----------All Errors minus 404
+function bigError(error, res) {
+    console.log(error);
+    res.status(error).send('BROKEN!');
+}
+
+//----------Connect to Server and Database
+database.connect(() => {
+    app.listen(PORT, () => console.log(`WORKING!: ${PORT}`));
+})
