7fb16b355ec6e2527e21cbdd0a47f3b56ce1dca6
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-07-21 04:39:07 -0700

    the ability to fill out user name and password and log the data into a table in the master database is working

--- schema/pokemon.sql
+++ schema/pokemon.sql
@@ -3,5 +3,14 @@ DROP TABLE IF EXISTS pokemon;
 CREATE TABLE pokemon
 (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(225)
+  name VARCHAR(225),
+  UNIQUE (name)
+);
+
+CREATE TABLE accounts
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(225),
+  password VARCHAR(225),
+  UNIQUE (name)
 );
